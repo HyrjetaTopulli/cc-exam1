10648765a85d0e3252ba79068486a1ba1365fd93
Hyrjeta Topulli <88555004+HyrjetaTopulli@users.noreply.github.com>
2026-01-22 17:25:32 +0100

    Refactor ft_strdup and add comments
    Refactored ft_strdup function to improve readability and added comments for clarity.

--- level2/ft_strdup.c
+++ level2/ft_strdup.c
@@ -1,61 +1,33 @@
-// #include <stdlib.h>
-
-// char *ft_strdup(char *src)
-// {
-//     char *temp;
-//     int len = 0;
-//     int i = 0;
-
-//     while(src[len])
-//         len++;
-
-//     temp = malloc(sizeof(char) * len + 1);
-
-//     if (!temp)
-//         return NULL;
-
-//     while (src[i])
-//     {
-//         temp[i] = src[i];
-//         i++;
-//     }
-//     temp[i] = '\0';
-//     return temp;
-// }
-
-
-
-// #include <stdio.h>
-
-// int main()
-// {
-//     char *a;
-//     a = "hello world";
-//     printf("%s\n", ft_strdup(a));
-// }
-
-
 #include <stdlib.h>
 
 char *ft_strdup(char *src)
 {
-    char *temp;
-    int len = 0;
     int i = 0;
+    int len = 0;
+    char *temp; // we'll duplicate string into this variable
 
-    while(src[len])
+    while(src[len]) //we calculate the length of the string to create enough space in memory
         len++;
-    
-    temp = malloc(sizeof(char) * len + 1);
 
+    temp = malloc(sizeof(char) * (len + 1)); //allocate space with malloc, and add +1 cuz the string needs one extra bit for "\0"
     if (!temp)
         return NULL;
-    
-    while(src[i])
+
+    while (src[i]) //loop to complete the new string
     {
         temp[i] = src[i];
         i++;
     }
-    temp[i] = 0;
-    return temp;
+    temp[i] = 0;  //add the new character at the end
+    return temp; // return the duplicated string
+}
+
+
+#include <stdio.h>
+
+int main()
+{
+	char	*a;
+	a = "Hello World";
+	printf("%s\n", ft_strdup(a));
 }

9a7486c90a226416c9fc3dcf5aa4ff33e8e97dfb
Hyrjeta Topulli <88555004+HyrjetaTopulli@users.noreply.github.com>
2026-01-22 15:28:23 +0100

    Fix atoi function to correctly calculate result

--- level2/ft_atoi.c
+++ level2/ft_atoi.c
@@ -17,7 +17,7 @@ int ft_atoi(const char *str)
 
     while (*str >= '0' && *str <= '9')
     {
-        result = result * 10 - (*str - '0');
+        result = result * 10 + (*str - '0');
         str++;
     }
 

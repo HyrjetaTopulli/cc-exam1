832d5ca54e3e53a5303b2bfe1e50250a71c3006a
David Guri <81036791+davidguri@users.noreply.github.com>
2026-05-21 17:21:08 +0200

    Refactor main function and variable names

--- level2/inter.c
+++ level2/inter.c
@@ -1,29 +1,31 @@
 #include <unistd.h>
 
-int main (int argc, char *argv[])
-{
+int main(int argc, char **argv) {
     int i = 0;
     int j;
-    int ascii[128] = {0};
+    int char[128] = {0};
 
-
-    if (argc == 3)
-    {
-        while (argv[1][i])
-        {
-            j = 0;
-            while (argv[2][j])
-            {
-                if(argv[1][i] == argv[2][j] && !ascii[(int)argv[1][i]])
-                {
-                    ascii[(int)argv[1][i]] = 1;
-                    write(1, &argv[1][i], 1);
-                    //break;
-                }
-                j++;
-            }
-            i++;
-        }
+    if (argc != 3) {
+        write(1, "\n", 1);
+        return 0;
     }
+
+    while (argv[1][i]) {
+        j = 0;
+
+        while (argv[2][j]) {
+            if (argv[1][i] == argv[2][j] && !ascii[(int)argv[1][i]]) {
+                char[(int) argv[1][i]] = 1;
+                write(1, &argv[1][i], 1);
+            }
+
+            j++;
+        }
+
+        i++;
+    }
+
     write(1, "\n", 1);
+
+    return 0;
 }
